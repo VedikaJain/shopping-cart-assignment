a2a16b4192519e72b67840b7806178a7b28888f5
Vedika Jain <vedjain@publicisgroupe.net>
2019-12-04 17:20:16 +0530

    Implemented mobile first layout in products

--- src/Common/Widgets/Product/Product.js
+++ src/Common/Widgets/Product/Product.js
@@ -5,14 +5,40 @@ import PinkButton from '../Buttons/PinkButton/PinkButton';
 function Product(props) {
   return (
     <div className="Product">
-      <h3>{props.product.name}</h3>
-      <img src={process.env.PUBLIC_URL + props.product.imageURL}
-        alt={props.product.name} className="prodImg"/>
-      <p>{props.product.description}</p>
-      <div>
-        <span>MRP Rs.{props.product.price}</span>
-        <PinkButton className="pinkBtn" text='Buy Now'/>
+
+      <div className='isMobile'>
+        <h3>{props.product.name}</h3>
+        <div className='row'>
+          <img src={process.env.PUBLIC_URL + props.product.imageURL}
+          alt={props.product.name} className="prodImg" />
+          <div>
+            <p>{props.product.description}</p>
+            <PinkButton className="pinkBtn" text={'Buy Now @ Rs.' + props.product.price}/>
+          </div>
+        </div>
       </div>
+
+      <div className='isTablet'>
+        <h3>{props.product.name}</h3>
+        <div className='row'>
+          <img src={process.env.PUBLIC_URL + props.product.imageURL}
+          alt={props.product.name} className="prodImg" />
+          <p>{props.product.description}</p>
+        </div>
+        <PinkButton className="pinkBtn" text={'Buy Now @ Rs.' + props.product.price}/>
+      </div>
+
+      <div className='isLaptop'>
+        <h3>{props.product.name}</h3>
+        <img src={process.env.PUBLIC_URL + props.product.imageURL}
+          alt={props.product.name} className="prodImg" />
+        <p>{props.product.description}</p>
+        <div className='row'>
+          <span>MRP Rs.{props.product.price}</span>
+          <PinkButton className="pinkBtn" text='Buy Now' />
+        </div>
+      </div>
+
     </div>
   );
 }
