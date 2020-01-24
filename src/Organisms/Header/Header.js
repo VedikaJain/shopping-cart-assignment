cecd9ce67006fc282e1307d7c27b7256fb93fbcd
Vedika Jain <vedjain@publicisgroupe.net>
2020-01-24 16:21:13 +0530

    fixes

--- src/Organisms/Header/Header.js
+++ src/Organisms/Header/Header.js
@@ -32,8 +32,7 @@ function Header(props) {
       <header className='header'>
         <img alt={Constants.Logo} className='header__logo'
           src={Constants.UrlPublic + Constants.ImgLogo}
-          srcSet={Constants.UrlPublic + Constants.ImgLogo + ' 1x, '
-            + Constants.UrlPublic + Constants.ImgLogo2x + ' 2x'} />
+          srcSet={Constants.UrlPublic + Constants.ImgLogo2x + ' 2x'} />
         {(props.screenSize === Constants.ScreenMobile)
           && <nav className='header__nav'>
             <IconButton type={Constants.IconMenu}
